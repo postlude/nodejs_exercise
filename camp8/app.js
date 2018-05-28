45f5d39b2de1f5ed03fe1413b49556d069961024
postlude <seungdeokhan@naver.com>
2018-05-28 22:45:38 +0900

    4회차 수업

--- camp8/app.js
+++ camp8/app.js
@@ -1,7 +1,14 @@
 var express = require('express');
+// 반드시 이 위치에
+var path = require('path');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 //MongoDB 접속
 var mongoose = require('mongoose');
+// mongoose promise 에러 처리
+// promise 가 deprecation 되었으므로 다른것으로 교체 바람
+mongoose.Promise = global.Promise;
 var autoIncrement = require('mongoose-auto-increment');
  
 var db = mongoose.connection;
@@ -20,6 +27,17 @@ var admin = require('./routes/admin');
 var app = express();
 var port = 3000;
 
+// 현재 디렉토리 위치
+// console.log(__dirname);
+// 확장자가 ejs 로 끝나는 뷰 엔진을 추가한다.
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+// 반드시 이 위치에(port 아래)
+
+// 미들웨어 셋팅
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function(req, res){
     res.send('first edit');
--- camp8/routes/admin.js
+++ camp8/routes/admin.js
@@ -1,12 +1,69 @@
 var express = require('express');
 var router = express.Router();
+var ProductsModel = require('../models/ProductsModel');
 
 router.get('/', function(req, res){
     res.send('admin app1111');
 });
 
 router.get('/products', function(req, res){
-    res.send('admin product');
+    // res.send('admin product');
+
+    // views 폴더 하위로 적어준다.
+    // 변수 명은 "" 여부 상관없음, 뒤에는 반드시 해야함
+    // res.render('admin/products', {message : "hello", "camp" : "nodejs"});
+
+    ProductsModel.find(function(err, products){
+        res.render('admin/products', {products : products});
+    });
+});
+
+router.get('/products/write', function(req, res){
+    res.render('admin/form', {product : ""});
+});
+
+router.post('/products/write', function(req, res){
+    var product = new ProductsModel({
+        name : req.body.name,
+        price : req.body.price,
+        description : req.body.description
+    });
+    product.save(function(err){
+        res.redirect('/admin/products');
+    });
+});
+
+router.get('/products/detail/:id', function(req, res){
+    // url에서 변수 값을 받아올 땐 req.params.id 로 받아온다.
+    ProductsModel.findOne( { 'id' :  req.params.id } , function(err, product){
+        res.render('admin/productsDetail', { product: product });  
+    });
+});
+
+router.get('/products/edit/:id', function(req, res){
+    ProductsModel.findOne( { 'id' :  req.params.id } , function(err, product){
+        res.render('admin/form', { product: product });  
+    });
+});
+
+router.post('/products/edit/:id', function(req, res){
+    var query = {
+        name : req.body.name,
+        price : req.body.price,
+        description : req.body.description
+    };
+
+    // update의 첫번째 인자는 조건, 두번째 인자는 바꿀 값
+    ProductsModel.update({id : req.params.id}, {$set : query}, function(err){
+        // 수정 후 상세페이지로
+        res.redirect('/admin/products/detail/' + req.params.id);
+    });
+});
+
+router.get('/products/delete/:id', function(req, res){
+    ProductsModel.remove({id : req.params.id}, function(err){
+        res.redirect('/admin/products');
+    });
 });
 
 module.exports = router;
