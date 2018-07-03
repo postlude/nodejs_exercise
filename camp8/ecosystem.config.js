916d2d6014d1feaefc25d066554f80a54baefc57
postlude <seungdeokhan@naver.com>
2018-07-03 18:11:40 +0900

    13회차 복습

---
+++ camp8/ecosystem.config.js
@@ -0,0 +1,28 @@
+/*
+개발모드로 실행
+pm2 start ecosystem.config.js
+
+배포모드로 실행
+pm2 start ecosystem.config.js --env production
+*/
+
+module.exports = {
+    apps: [
+        {
+            // pm2로 실행한 프로세스 목록에서 이 애플리케이션의 이름으로 지정될 문자열
+            name: "app",
+            // pm2로 실행될 파일 경로
+            script: "./app.js",
+            // 개발환경시 적용될 설정 지정
+            env: {
+                "PORT": 3000,
+                "NODE_ENV": "development"
+            },
+            // 배포환경시 적용될 설정 지정
+            env_production: {
+                "PORT": 3000,
+                "NODE_ENV": "production"
+            }
+        }
+    ]
+};
