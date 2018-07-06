17c97670c5ba042e5f58f57e9c34085bd9138bd8
postlude <seungdeokhan@naver.com>
2018-07-06 11:46:51 +0900

    admin1 -> admin

--- camp8/libs/adminRequired.js
+++ camp8/libs/adminRequired.js
@@ -2,7 +2,7 @@ module.exports = function(req, res, next){
     if (!req.isAuthenticated()){ 
         res.redirect('/accounts/login');
     }else{
-        if(req.user.username !== 'admin1'){
+        if(req.user.username !== 'admin'){
             res.send('<script>alert("관리자만 접근 가능합니다.");location.href="/accounts/login";</script>');
         }else{
             next();
